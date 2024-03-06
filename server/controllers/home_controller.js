c2540c73dac6401b6f9439729025203c34d6725f
damuz91 <damuz91@gmail.com>
2024-03-06 07:29:23 -0500

    Agregada logica de escribir archivo

--- server/controllers/home_controller.js
+++ server/controllers/home_controller.js
@@ -1,10 +1,22 @@
 // Import any required services or models here
 // const homeService = require('../services/home_service');
+const fs = require('fs');
 
 exports.create_legal = async (req, res) => {
   try {
-    debugger;
     const { params } = req.params;
+    contenido = req.body.texto_creado;
+    
+    nombre_archivo = 'legal.txt';
+    // Write to the file
+    fs.writeFile(nombre_archivo, contenido, (err) => {
+      if (err) {
+        console.error('Error writing to file:', err);
+      } else {
+        console.log('File has been successfully written.');
+      }
+    });
+
     // const newExample = await exampleService.createExample(name);
     console.log("He recibido desde el frontend: ", req.body.texto_creado);
     res.json({success: true, mensage: 'Legal creado'});
@@ -12,3 +24,5 @@ exports.create_legal = async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
+
+
